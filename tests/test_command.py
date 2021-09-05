3bf3338a7aa26c31e29ab3c65a521124b439824d
sascha.desch@hotmail.com <sascha.desch@hotmail.com>
2021-09-05 12:37:40 +0200

    test: add test for correct dumping of command parts

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,5 +1,6 @@
 import pytest
 from hypothesis import given
+import hypothesis.strategies as st
 from .strategies import type_and_value, complex_type
 
 from reddish._command import Command
@@ -8,6 +9,14 @@ from reddish._command import Command
 def test_command_parses_data(example):
     type_, value = example
     assert value == Command().into(type_)._parse_response(value)
+
+@given(example=complex_type.flatmap(st.from_type))
+def test_command_dump_parts(example):
+    cmd = Command(example)
+    dump = cmd._dump_parts()
+    assert len(dump) == 1, "dumped to the wrong number of parts"
+    assert isinstance(dump[0], bytes), "dumped to the wrong type"
+
 
 def test_command_subclass():
 
