7ee0ffb67a1da07acb24c8d2c2262ad5e1ef2317
sascha.desch@hotmail.com <sascha.desch@hotmail.com>
2021-11-03 19:49:21 +0100

    add apply_template method for converting template string and args/kwargs into list of arguments

--- tests/test_templating.py
+++ tests/test_templating.py
@@ -1,11 +1,48 @@
-from reddish._templating import parse_command_template
+import pytest
+from reddish._templating import parse_command_template, apply_template, format_original_field
 
 
 def test_positional_args():
-    # (text, field, spec, conversion)
-    assert [('', 0, '', None), ('', 1, '', None)] == list(parse_command_template('{}{}'))
+
+    assert (
+        # (text, field, spec, conversion)
+        [('', 0, '', None), ('', 1, '', None)] 
+        == list(parse_command_template('{}{}'))
+        == list(parse_command_template('{0}{1}'))
+        )
 
 
 def test_keyword_args():
     # (text, field, spec, conversion)
     assert [('', 'foo', '', None), ('', 'bar', '', None)] == list(parse_command_template('{foo}{bar}'))
+
+
+def test_apply_template_with_positional_args():
+    assert (
+        ['SET', 'foo', 'bar']
+        == apply_template('SET {} {}', 'foo', 'bar')
+        == apply_template('SET {0} {1}', 'foo', 'bar')
+    )
+
+
+def test_apply_template_with_keyword_args():
+    assert ['SET', 'foo', 'bar'] == apply_template('SET {foo} {bar}', foo='foo', bar='bar')
+
+
+def test_format_original_field():
+    assert 'foo!s:f' == format_original_field('foo', 'f', 's')
+
+
+def test_apply_template_with_modifiers():
+    with pytest.raises(ValueError):
+        apply_template('{foo!s:f}')
+
+
+def test_apply_template_with_missing_positional_args():
+    with pytest.raises(TypeError):
+        apply_template('{}{}', 1)
+
+
+def test_apply_template_with_missing_keyword_args():
+    with pytest.raises(TypeError):
+        apply_template('{foo}{bar}', foo=1)
