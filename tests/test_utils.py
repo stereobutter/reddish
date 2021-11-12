e9293b0238a5b11651358ddfa16ed84dff503b78
sascha.desch@hotmail.com <sascha.desch@hotmail.com>
2021-11-12 11:06:08 +0100

    test: basic test for uppercase_template_string

---
+++ tests/test_utils.py
@@ -0,0 +1,13 @@
+import pytest
+from reddish._utils import uppercase_template_string
+
+
+@pytest.mark.parametrize("template, expected",
+        [
+            ('echo {data}', 'ECHO {data}'),
+            ('echo', 'ECHO'),
+            ('echo {}', 'ECHO {}')
+        ]
+    )
+def test_uppercase_template_string(template, expected):
+    assert expected == uppercase_template_string(template)
