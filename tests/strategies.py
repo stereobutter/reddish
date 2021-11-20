6160a16c0efeeb002d995e15f259c80a0a637017
sascha.desch@hotmail.com <sascha.desch@hotmail.com>
2021-11-20 11:44:49 +0100

    replace the use generic collections with the classes from typing

--- tests/strategies.py
+++ tests/strategies.py
@@ -1,5 +1,4 @@
-from __future__ import annotations
-from typing import Union
+from typing import Union, List, Dict, Tuple
 
 import hypothesis.strategies as st
 
@@ -10,20 +9,20 @@ primitive_type = st.sampled_from([int, bool, str, type(None)])
 @st.composite
 def generic_list_type(draw, item_type_strategy):
     item_type = draw(item_type_strategy)
-    return list[item_type]
+    return List[item_type]
 
 
 @st.composite
 def generic_dict_type(draw, key_type_strategy, value_type_stategy):
     key_type = draw(key_type_strategy)
     value_type = draw(value_type_stategy)
-    return dict[key_type, value_type]
+    return Dict[key_type, value_type]
 
 
 @st.composite
 def union_type(draw, type_strategy):
     num = draw(st.integers(min_value=2, max_value=5))
-    return Union[tuple(draw(type_strategy) for _ in range(num))]
+    return Union[Tuple(draw(type_strategy) for _ in range(num))]
 
 
 complex_type = st.recursive(
