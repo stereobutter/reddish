869343703c86fa708f7db141c7ad2b7e60538df7
sascha.desch@hotmail.com <sascha.desch@hotmail.com>
2021-11-20 11:33:44 +0100

    add `from __future__ import annotations` required by python 3.7 and 3.8

--- reddish/_utils.py
+++ reddish/_utils.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import json
 from itertools import islice
 from typing import Iterable, Union
--- tests/strategies.py
+++ tests/strategies.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import Union
 
 import hypothesis.strategies as st
