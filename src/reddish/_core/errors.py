242be5e193884f5dbd0c5674fb8ede92a7a32632
sascha.desch@hotmail.com <sascha.desch@hotmail.com>
2023-03-19 12:12:17 +0100

    feat: add CommandError, PipelineError and TransactionError

--- src/reddish/_core/errors.py
+++ src/reddish/_core/errors.py
@@ -1,3 +1,7 @@
+from outcome import Outcome
+from typing import Iterable
+
+
 class BrokenConnectionError(Exception):
     def __init__(self, msg="Connection closed.", *args, **kwargs):
         super().__init__(msg, *args, **kwargs)
@@ -16,3 +20,32 @@ class CommandError(Exception):
 
     def __init__(self, reply: str):
         self.code, self.message = str(reply).split(maxsplit=1)
+
+
+class PipelineError(Exception):
+    """Raised when one or more pipelined commands resulted in an error.
+
+    The values and/or errors are made available as `Outcome` objects and can be
+    iterated over by iterating this exception.
+    """
+
+    def __init__(self, outcomes: Iterable[Outcome]):
+        self._outcomes = outcomes
+
+    def __iter__(self):
+        yield from self._outcomes
+
+
+class TransactionError(Exception):
+    """Raised when one or more commands in a Multi/Exec transaction
+    resulted in an error.
+
+    The values and/or errors are made available as `Outcome` objects and can be
+    iterated over by iterating this exception.
+    """
+
+    def __init__(self, outcomes: Iterable[Outcome]):
+        self._outcomes = outcomes
+
+    def __iter__(self):
+        yield from self._outcomes
