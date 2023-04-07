5b7848fa9cda1c87fa0f361120d7378b4d1c3058
Sascha Desch <sascha.desch@hotmail.com>
2023-04-07 16:41:45 +0200

    feat(typing): add type hints to parser module

--- src/reddish/_core/parser.py
+++ src/reddish/_core/parser.py
@@ -1,8 +1,11 @@
 from pydantic import parse_obj_as, ValidationError
 from reddish._core.errors import ParseError
+from typing import Type, Any, TypeVar
+
+T = TypeVar("T")
 
 
-def parse(type_, value):
+def parse(type_: Type[T], value: Any) -> T:
     try:
         return parse_obj_as(type_, value)
     except ValidationError as error:
