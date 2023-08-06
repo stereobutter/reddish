a19533499037768fa46083def5c2abb97451fe57
Sascha Desch <sascha.desch@hotmail.com>
2023-08-06 13:51:54 +0200

    feat: re-export symbols from __init__.py

--- src/reddish/__init__.py
+++ src/reddish/__init__.py
@@ -1,8 +1,5 @@
-from reddish._core.command import Args, Command  # noqa
-from reddish._core.multiexec import MultiExec  # noqa
-from reddish._core.errors import (  # noqa
-    CommandError,
-    ParseError,
-    PipelineError,
-    TransactionError,
-)
+from reddish._core.command import Args as Args, Command as Command
+from reddish._core.multiexec import MultiExec as MultiExec
+import reddish._core.errors as errors
+import reddish.backends as backends
+import reddish.models as models
