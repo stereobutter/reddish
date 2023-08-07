4ccc6d2c21eb0a9b6a59fe9e7f837d72d0f1f0dd
Sascha Desch <sascha.desch@hotmail.com>
2023-08-07 21:32:14 +0200

    test: add unit tests for asyncio backend

---
+++ tests/asyncio/test_asyncio.py
@@ -0,0 +1,57 @@
+import asyncio
+import pytest
+import pytest_asyncio
+from reddish.backends.asyncio import Redis
+from reddish import Command
+from reddish._core.errors import ConnectionError
+
+pytestmark = pytest.mark.asyncio
+
+
+@pytest_asyncio.fixture
+async def connection():
+    return asyncio.open_connection("localhost", 6379)
+
+
+@pytest_asyncio.fixture
+async def redis(connection):
+    return Redis(await connection)
+
+
+@pytest.fixture
+def ping():
+    return Command("PING").into(str)
+
+
+async def test_execute(redis, ping):
+    "PONG" == await redis.execute(ping)
+
+
+async def test_execute_many(redis, ping):
+    ["PONG", "PONG"] == await redis.execute_many(ping, ping)
+
+
+async def test_stream(connection):
+    with pytest.raises(TypeError):
+        Redis(connection)
+
+
+async def test_closed_connection(connection, ping):
+    reader, writer = await connection
+    writer.close()
+    redis = Redis((reader, writer))
+
+    with pytest.raises(ConnectionError):
+        await redis.execute(ping)
+
+
+async def test_concurrent_requests(redis, ping):
+    await asyncio.gather(*[redis.execute(ping) for _ in range(10)])
+
+
+async def test_cancellation(redis, ping):
+    # instructs redis server to close connection
+    await redis.execute(Command("QUIT"))
+
+    with pytest.raises(ConnectionError):
+        await redis.execute(ping)
