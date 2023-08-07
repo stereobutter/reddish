c41eab0ca7f3e508734aeaf1a3285c3a00352e34
Sascha Desch <sascha.desch@hotmail.com>
2023-08-07 21:41:52 +0200

    fix: make async test runners for asyncio and trio play nice together

--- tests/asyncio/test_asyncio.py
+++ tests/asyncio/test_asyncio.py
@@ -23,19 +23,23 @@ def ping():
     return Command("PING").into(str)
 
 
+@pytest.mark.asyncio
 async def test_execute(redis, ping):
     "PONG" == await redis.execute(ping)
 
 
+@pytest.mark.asyncio
 async def test_execute_many(redis, ping):
     ["PONG", "PONG"] == await redis.execute_many(ping, ping)
 
 
+@pytest.mark.asyncio
 async def test_stream(connection):
     with pytest.raises(TypeError):
         Redis(connection)
 
 
+@pytest.mark.asyncio
 async def test_closed_connection(connection, ping):
     reader, writer = await connection
     writer.close()
@@ -45,10 +49,12 @@ async def test_closed_connection(connection, ping):
         await redis.execute(ping)
 
 
+@pytest.mark.asyncio
 async def test_concurrent_requests(redis, ping):
     await asyncio.gather(*[redis.execute(ping) for _ in range(10)])
 
 
+@pytest.mark.asyncio
 async def test_cancellation(redis, ping):
     # instructs redis server to close connection
     await redis.execute(Command("QUIT"))
--- tests/trio/test_trio.py
+++ tests/trio/test_trio.py
@@ -21,19 +21,23 @@ def ping():
     return Command("PING").into(str)
 
 
+@pytest.mark.trio
 async def test_execute(redis, ping):
     "PONG" == await redis.execute(ping)
 
 
+@pytest.mark.trio
 async def test_execute_many(redis, ping):
     ["PONG", "PONG"] == await redis.execute_many(ping, ping)
 
 
+@pytest.mark.trio
 async def test_stream(connection):
     with pytest.raises(TypeError):
         Redis(connection)
 
 
+@pytest.mark.trio
 async def test_closed_connection(connection, ping):
     async with await connection as stream:
         redis = Redis(stream)
@@ -42,12 +46,14 @@ async def test_closed_connection(connection, ping):
         await redis.execute(ping)
 
 
+@pytest.mark.trio
 async def test_concurrent_requests(redis, ping):
     async with trio.open_nursery() as nursery:
         for i in range(10):
             nursery.start_soon(redis.execute, ping)
 
 
+@pytest.mark.trio
 async def test_cancellation(redis, ping):
     # instructs redis server to close connection
     await redis.execute(Command("QUIT"))
